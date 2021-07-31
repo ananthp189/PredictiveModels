3568accc41ced62b9583f12e5fab22473f62a362
ananthp189 <ananthrhul@gmail.com>
2021-07-31 21:24:27 +0100

    Binomial GLMS example and interpret

--- GLM/GLM_Binomial.R
+++ GLM/GLM_Binomial.R
@@ -31,3 +31,48 @@ cbind(beetles$killed, round(fitted,2))
   m2 <- glm(cbind(killed, number-killed) ~ dose, family = binomial(link = 'probit'),
             data=beetles)
 summary(m2)
+
+#Finally, a third choice of link that we could consider is the complementary log-log link, with the GLM equation given by
+m3 <- glm(cbind(killed, number-killed) ~ dose, family = binomial(link = 'cloglog'),
+          data=beetles)
+
+summary(m3)
+
+#plot fitted models
+beet_p <- data.frame(beetles = beetles,
+                     logit = fitted(m1),
+                     probit = fitted(m2),
+                     cloglog = fitted(m3))
+p2 <- ggplot(beet_p, aes(x = beetles$dose, y = beetles$propkilled)) +
+  geom_point() + xlab("Dose") + ylab("Proportion killed") +
+  geom_line(aes(x = beetles$dose, y = logit, colour = "Logit")) +
+  geom_line(aes(x = beetles$dose, y = probit, colour = "Probit")) +
+  geom_line(aes(x = beetles$dose, y = cloglog, colour = "C log-log")) +
+  guides(colour = guide_legend("Method"))
+p2
+
+library(faraway)
+head(orings)
+#logit link
+lmod <- glm(cbind(damage, 6-damage) ~ temp, family=binomial, data=orings)
+summary(lmod)
+#Probit link:
+  pmod <- glm(cbind(damage, 6-damage) ~ temp, family=binomial(link="probit"),
+              data=orings)
+summary(pmod)
+#Complementary log-log link:
+  cmod <- glm(cbind(damage, 6-damage) ~ temp, family=binomial(link="cloglog"), data=orings)
+summary(cmod)
+
+
+pred1 <- predict(lmod, newdata=data.frame(temp=seq(25,85,le=23)), type="response")
+pred2 <- predict(pmod, newdata=data.frame(temp=seq(25,85,le=23)), type="response")
+pred3 <- predict(cmod, newdata=data.frame(temp=seq(25,85,le=23)), type="response")
+pred <- data.frame(logit = pred1, probit= pred2, cloglog=pred3, px = seq(25,85,le=23),orings)
+p1.1 <- ggplot(pred, aes(x=orings$temp, y= orings$damage/6)) +
+  geom_point(size = 1)+ xlim (c(25,85)) + ylim(c(0,1)) +
+  xlab ("Temperature (F)") + ylab("Probability of damage") +
+  geom_line(aes(x = px, y = logit, color = "Logit")) +
+  geom_line(aes(x = px, y = probit, color = "Probit"))+
+  geom_line(aes(x = px, y = cloglog, color = "Complementary log-log"))
+p1.1
